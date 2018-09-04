ae8608d188b937bd239495d5141ff197952847eb
Evgeniy <evgeniy56@orsk.ru>
2018-09-04 08:14:43 +0500

    Edit dz-07 01

--- dz-07/01.sh
+++ dz-07/01.sh
@@ -3,3 +3,12 @@
 #
 #
 
+
+printf "%+5s %-8s %-6s %04s %-s\n" "PID" "TTY" "STAT" "TIME" "COMMAND"
+
+for DirProcess in `ls -1 /proc | grep -E ^\-?[0-9]?\.?[0-9]+$  | sort -g`
+do
+	if [ -d /proc/$DirProcess ]; then
+		printf "%5d %-8s %-6s %04s %-s\n" $DirProcess "?" "Is<" "0:00" "systemd"
+	fi	
+done
