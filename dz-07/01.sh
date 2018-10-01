149348340f4449cb64423a12542789ec53f962ca
Evgeniy <evgeniy56@orsk.ru>
2018-10-01 18:32:45 +0500

    Edit dz-07 02

--- dz-07/01.sh
+++ dz-07/01.sh
@@ -3,12 +3,47 @@
 #
 #
 
+PrintCommandf(){
+	if [ -f /proc/$1/cmdline ]; then
+		commen=$(cat /proc/$1/cmdline | tr -d "\0" | tr -s "^@" "\n")
+		if [ ${#commen} -eq 0 ]; then
+			commen="["$(cat /proc/$1/comm | tr -d "\0" | tr -s "^@" "\n")"]"
+		fi
+	fi
+	echo $commen
+
+}
+
+PrintSatatus(){
+	state=`awk '{ print $3}' /proc/$1/stat`
+	echo $state
+}
+
+PrintTTY(){
+	numdives=`awk '{ print $7}' /proc/$1/stat`
+	if [ $numdives -eq 0 ]; then		
+		echo "?"
+	else
+		echo $numdives
+	fi
+}
+PrintTime(){
+
+	echo "0:00"
+}
 
 printf "%+5s %-8s %-6s %04s %-s\n" "PID" "TTY" "STAT" "TIME" "COMMAND"
 
 for DirProcess in `ls -1 /proc | grep -E ^\-?[0-9]?\.?[0-9]+$  | sort -g`
 do
 	if [ -d /proc/$DirProcess ]; then
-		printf "%5d %-8s %-6s %04s %-s\n" $DirProcess "?" "Is<" "0:00" "systemd"
+		printComm=$(PrintCommandf $DirProcess)
+		printTTY=$(PrintTTY $DirProcess)
+		printSatatus=$(PrintSatatus $DirProcess)
+		printf "%5s %-8s %-6s %04s %s\n" $DirProcess "$printTTY" "$printSatatus" "0:00" "$printComm" | head -c `tput cols`
+#		printf "%b\n" "$printComm" | head -c `tput cols`
 	fi	
 done
+
+#tr -s "^@" "\n"
+#head -c `tput cols`
